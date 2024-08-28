6cda3b1f81d8fe772db34cdbec3fc8676df87775
jbenede <jesbegar@gmail.com>
2024-08-28 13:16:20 +0200

    ⬆️ (package.json, project.json): remove `zone.js` dependency and update app configuration for zoneless change detection
    ♻️ (app.config.ts): refactor app configuration to use experimental zoneless change detection and update import formatting
    
    Removing `zone.js` from dependencies and the project configuration aligns with the shift towards a zoneless Angular application, aiming for performance improvements by reducing the need for Angular to run change detection across the entire app whenever asynchronous tasks complete. This change necessitates the update in `app.config.ts` to use `provideExperimentalZonelessChangeDetection` instead of `provideZoneChangeDetection`, which is part of Angular's efforts to enable more fine-grained control over performance optimizations.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideRouter } from '@angular/router';
-import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideAnimations(),
-        provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-        NG_EVENT_PLUGINS
-    ],
+    provideAnimations(),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes, withComponentInputBinding()),
+    NG_EVENT_PLUGINS,
+    provideExperimentalZonelessChangeDetection(),
+  ],
 };
