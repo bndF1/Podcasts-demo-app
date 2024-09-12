4ce53b1078cc899437bb9033fefd10830aa98a55
jbenede <jesbegar@gmail.com>
2024-09-12 09:40:00 +0200

    ⬆️ (package.json): upgrade Nx dependencies to version 19.7.2 ✨ (episodes.service.ts): add type annotations and import Signal for better type safety
    Upgrading Nx dependencies ensures compatibility with the latest features and fixes, improving overall stability and performance. Adding type annotations and importing `Signal` in `episodes.service.ts` enhances type safety and code clarity, making it easier to maintain and understand.

--- src/app/services/episodes/episodes.service.ts
+++ src/app/services/episodes/episodes.service.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { EpisodeAdapter } from 'src/app/adapters/episode.adapter';
-import { Chapter } from 'src/app/models';
+import { Chapter, Episode } from 'src/app/models';
 import { ApiService } from '../api.service';
 
 @Injectable({
@@ -13,7 +13,7 @@ export class EpisodesService {
 
   readonly #api = inject(ApiService);
 
-  getAllEpisodes() {
+  getAllEpisodes(): Signal<Episode[]> {
     return toSignal(
       this.#api.get<Chapter[]>(this.path).pipe(map(EpisodeAdapter)),
       { initialValue: [] },
