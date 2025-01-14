93012ec1a969d70b27bacf13d36054f8a6dee49b
jbenede <jesbegar@gmail.com>
2025-01-14 14:59:55 +0100

    fix(navigation): update Queue item to indicate it's a todo

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -70,10 +70,12 @@ import { map } from 'rxjs';
               </li>
               <li>
                 <a
-                  class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  class="cursor-not-allowed flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <span class="material-symbols-outlined"> queue_music </span>
-                  <span class="flex-1 ms-3 whitespace-nowrap">Queue</span>
+                  <span class="flex-1 ms-3 whitespace-nowrap"
+                    >Queue (todo)</span
+                  >
                   <span
                     class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
                     >3</span
